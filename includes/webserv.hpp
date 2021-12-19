8c2d06f6a970ca5bae1c2241344d14dcc35b6a2a
3lswear <r.malyavsky@protonmail.com>
2021-12-19 16:45:43 +0300

    hello world

---
+++ includes/webserv.hpp
@@ -0,0 +1,16 @@
+#ifndef WEBSERV_HPP
+#define WEBSERV_HPP
+
+#include <iostream>
+#include <string>
+#include <arpa/inet.h>
+#include <sys/select.h>
+#include <poll.h>
+#include <sys/epoll.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#endif
