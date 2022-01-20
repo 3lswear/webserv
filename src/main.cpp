eedb871c766babae154dc51a15ed30a3e3a7fc1e
Talyx <farruh.rahmatov40@bk.ru>
2022-01-20 13:54:58 +0300

    comment

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@ int	main(int argc, char **argv)
 
 	Server	server;
 	
-	/* server.readConfig(); */
-	/* server.setupConfig(); */
-	/* server.start(); */
-	parse();
+	 server.readConfig(); 
+	 server.setupConfig();
+	 server.start();
+	// parse();
 	
 
 
