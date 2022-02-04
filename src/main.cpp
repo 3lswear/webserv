baada770c9ddb6ffa1505255002db5316a37adbc
3lswear <r.malyavsky@protonmail.com>
2022-02-04 18:17:04 +0300

    feat: add DBOUT define

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,14 @@
 #include "parse.hpp"
 #include "Server.hpp"
 
+#ifdef DEBUG
+#define DBOUT std::cerr << TURQ
+#define ENDL RESET << std::endl
+#else
+#define DBOUT 0 && std::cerr
+#define ENDL RESET << std::endl
+#endif
+
 int	main(int argc, char **argv)
 {
 	(void)argc;
