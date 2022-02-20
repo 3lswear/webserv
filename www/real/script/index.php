4f0173ea08b032a1b38e5705e1ddce4db87fb8d2
Talyx <farruh.rahmatov40@gmail.com>
2022-02-20 19:58:00 +0300

    new config

---
+++ www/real/script/index.php
@@ -0,0 +1,14 @@
+<?php
+
+
+    phpinfo();
+        // echo ("Content-type:text/html\r\n\r\n");
+        // echo ("<html>\n");
+        // echo ("<head>\n");
+        // echo ("<title>Hello World - First CGI Program</title>\n");
+        // echo ("</head>\n");
+        // echo ("<body>\n");
+        // echo ("<h2>Hello World! This is my first CGI program</h2>\n");
+        // echo ("</body>\n");
+        // echo ("</html>\n");
+?>
